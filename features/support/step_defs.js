fd7477013c44629d02175f36d68f6fc1aece419a
Kate <vo.yen@mulodo.com>
2016-08-02 19:32:36 +0700

    update features

--- features/support/step_defs.js
+++ features/support/step_defs.js
@@ -4,15 +4,6 @@ module.exports = function() {
     browser.url('http://localhost:3000');
   });
 
-  this.When(/^I search for "([^"]*)"$/, function (searchTerm) {
-    browser.setValue('input[name="q"]', searchTerm);
-    browser.keys(['Enter']);
-  });
-
-  this.Then(/^I see "([^"]*)"$/, function (searchTerm) {
-    expect(browser.getText('h3')).toEqual(searchTerm);
-  });
-
   this.When(/^I fill name is "([^"]*)"$/, function (name) {
     browser.setValue('input[name="name"]', name);
   });
@@ -25,9 +16,12 @@ module.exports = function() {
     browser.click('button');
   });
 
+  this.Then(/^I see "([^"]*)"$/, function (searchTerm) {
+    expect(browser.getText('h3')).toEqual(searchTerm);
+  });
+
   this.Then(/^I see "([^"]*)" and "([^"]*)" were input before$/, function (name,quote) {
     var span = browser.getText('span');
-    console.log(span);
     expect(span[span.length-2]).toEqual(name);
     expect(span[span.length-1]).toEqual(quote);
   });
